b8997a0dc581e27f88645290545d5425ef6f446b
Johnny Garcia <jhonnygar98@hotmail.com>
2021-04-14 15:08:28 -0500

    Update the example asyn config

--- src/config/async-main.php
+++ src/config/async-main.php
@@ -3,7 +3,6 @@
 return [
   'id' => 'app-async',
   'basePath' => dirname(__DIR__),
-  'bootstrap' => ['log'],
   'controllerNamespace' => 'frontend\controllers',
   'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
   'aliases' => [
@@ -11,15 +10,6 @@ return [
     '@npm'   => '@vendor/npm-asset',
   ],
   'components' => [
-    'log' => [
-      'traceLevel' => YII_DEBUG ? 3 : 0,
-      'targets' => [
-        [
-          'class' => 'yii\log\FileTarget',
-          'levels' => ['error', 'warning'],
-        ],
-      ],
-    ],
     'cache' => [
       'class' => 'yii\caching\FileCache',
     ],
